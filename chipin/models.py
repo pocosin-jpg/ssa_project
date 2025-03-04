4c84270ee92969e83e4d1840b5aa98fa76d9ded7
poco <133003205+pocosin-jpg@users.noreply.github.com>
2025-03-05 00:39:11 +1100

    Implemented the voting system

--- chipin/models.py
+++ chipin/models.py
@@ -18,3 +18,15 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class GroupJoinRequest(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, related_name="join_requests"
+    )
+    is_approved = models.BooleanField(default=False)
+    votes = models.ManyToManyField(
+        CustomUser, related_name="votes", blank=True
+    )  # Tracks users who voted
+    created_at = models.DateTimeField(auto_now_add=True)
